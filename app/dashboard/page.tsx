3ce80cbea910d29b8c45e0be8de3061d576d6d30
Nguyen Hiep <nhh.tcp@gmail.com>
2024-02-29 20:54:40 +0700

    fix1

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,7 +10,7 @@ export default async function Page(){
         <main>
             <h1 className={`${fontABeeze.className} mb-4 text-lg md:text-2xl`}>Thống kê</h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
-            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 bg-yellow-600 lg:grid-cols-8">
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                   {/* <RevenueChart revenue={revenue}  /> */}
             </div>
         </main>
